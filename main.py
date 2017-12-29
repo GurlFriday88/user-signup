7b34877d7a43141341eef690891784043ce5ccf9
GurlFriday88 <entrotlek@gmail.com>
2017-12-29 15:50:56 -0500

    got render template working in index route

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect
 
 app=Flask(__name__)
 
@@ -7,5 +7,6 @@ app.config['DEBUG']=True
 
 @app.route('/home')
 def make_home():
-    return render_template(home.html)
+    return render_template('home.html', title="User signup")
 
+app.run()
