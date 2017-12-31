a01828c64029c12fa4ebd929fab92c2e82017ad2
GurlFriday88 <entrotlek@gmail.com>
2017-12-31 13:47:40 -0500

    Added string formating brackets to home form for values and error messages

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def is_empty_field(form_value):
     #takes value of form and checks to see if it is an empty string
     #if it is empty string then display error message
     #error message
-    error= "Missing information. Please enter username password and verify password"
+    error= "Missing information. Please enter username, password and verify password"
     #else pass if all fields are present 
     else:
         return True
@@ -21,9 +21,10 @@ def make_home():
     return render_template('home.html', title="User signup")
 
 #processes form to check for correct input length an to ensure there are no spaces
-@app.route('/home')
+@app.route('/home' methods=['GET , POST'])
 def process_form():
     # need to make conditional to process form if method is get or post 
+    if methods==POST
     user_name= request.form('user_name') #variables to contain form inputs
     password= request.form('password')
     verify_password= request.form('verify_password')
