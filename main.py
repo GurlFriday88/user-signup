95dbf6055645bf6a2d5a7dc6c4192e741729a113
GurlFriday88 <entrotlek@gmail.com>
2018-01-06 13:38:01 -0500

    fixed username and password bugs. currently working on email validation bug

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template, redirect
+import re
 
 app=Flask(__name__)
 
@@ -12,58 +13,58 @@ def validate(form_input):
     if not form_input:
         error = "Enter {name}"
 
-    elif len(form_value) < 2 or len(form_value) > 19:  # check for length
+    elif len(form_value) < 3 or len(form_value) > 20:  # check for length
         error = "Enter a value between (3-20) characters"
 
-    elif "" in form_value:  # check for spaces
+    elif " " in form_value:  # check for spaces
         error = "Enter {name} without spaces"
-    return error.format(name="form_input")
+    return error.format(name="form_value")
     
-    return error
+   
 
 
 #routes to home screen showing form 
 @app.route('/', methods=['POST','GET'])
 def make_home():
     if request.method == 'POST':
-        # # variables to contain form inputs
+        # # # variables to contain form inputs
         user_name = request.form['user_name']
         password = request.form['password']
         verify_password = request.form['verify_password']
         email = request.form['email']
-        #variables to contain error messages
+        # #variables to contain error messages
         user_name_error=""
         password_error=""
         verify_pass_error=""
         compare_pass_error=""
         email_error=""
-        #checks for validation of username, password, and verify pass
+        # #checks for validation of username, password, and verify pass
         user_name_error= validate(user_name)
         password_error= validate(password)
         verify_pass_error= validate(verify_password)
         
-        #compare password to verify password
+        # #compare password to verify password
         if verify_password not in password:
             compare_pass_error="Password confirmation does not match"
         #check for email input
-        if email == email:
+        if email:
             #check for @ and . symbols
-            if "@" or ".com" not in email:
-                email_error="Email must contain both @ and ."
+            if not re.search(r"([a-z]+[@]+[a-z]+[.]+[a-z])", email):
+                email_error="Email must contain both @ and .com"
             else:
                 email_error=validate(email) #complete email validation 
         #checks for empty error messages. If the validation passed the welcome page will be generated 
-        if not (user_name_error,
-                password_error,
-                verify_pass_error,
-                compare_pass_error,
-                email_error,
+        if not (user_name_error or
+                password_error or
+                verify_pass_error or 
+                compare_pass_error or
+                email_error
             ):
             return redirect('welcome?user_name={0}'.format(user_name))
         return render_template('home.html',
                                username_error=user_name_error,
                                password_error=password_error,
-                               verify_password_error=verify_pass_error,
+                               verify_pass_error=verify_pass_error,
                                compare_pass_error=compare_pass_error,
                                email_error=email_error,
                                 user_name=user_name,
