cadc27b5e48dabea6a7f53a8f1375af2f1de3de6
GurlFriday88 <entrotlek@gmail.com>
2018-01-08 02:25:45 -0500

    added bootstrap template and css for index and welcome page. Project fully functional and complete

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ def validate(form_input):
     form_value = form_input
     error = ''
     if not form_input:
-        error = "Enter {name}"
+        error = "Why you no enter {name} !?"
 
     elif len(form_value) < 3 or len(form_value) > 20:  # check for length
-        error = "Enter a value between (3-20) characters"
+        error = "I need something between (3-20) characters"
 
     elif " " in form_value:  # check for spaces
-        error = "Enter {name} without spaces"
+        error = "Me no likey {name} with spaces"
     return error
     
    
@@ -27,7 +27,7 @@ def validate(form_input):
 @app.route('/', methods=['POST','GET'])
 def make_home():
     if request.method == 'POST':
-        # # # variables to contain form inputs
+        # variables to contain form inputs
         user_name = request.form['user_name']
         password = request.form['password']
         verify_password = request.form['verify_password']
@@ -38,19 +38,19 @@ def make_home():
         verify_pass_error=""
         compare_pass_error=""
         email_error=""
-        # #checks for validation of username, password, and verify pass
+        # #checks for validation of username, password, and verify password
         user_name_error= validate(user_name).format(name="Username")
         password_error = validate(password).format(name="Password")
         verify_pass_error = validate(verify_password).format(name="Verify Password")
         
         # #compare password to verify password
         if verify_password not in password:
-            compare_pass_error="Password confirmation does not match"
+            compare_pass_error="NONE SHALL PASS!...Without matching passwords"
         #check for email input
         if email:
             #check for @ and . symbols
             if not re.search(r"([a-z]+[@]+[a-z]+[.]+[a-z])", email):
-                email_error="Email must contain a domain name, @ and ."
+                email_error="Can I get an actual email address?"
             else:
                 email_error = validate(email).format(
                     name="Email")  # complete email validation
@@ -62,7 +62,7 @@ def make_home():
                 email_error
             ):
             return redirect('welcome?user_name={0}'.format(user_name))
-        return render_template('home2.html',
+        return render_template('signup-page.html',
                                username_error=user_name_error,
                                password_error=password_error,
                                verify_pass_error=verify_pass_error,
@@ -70,7 +70,7 @@ def make_home():
                                email_error=email_error,
                                 user_name=user_name,
                             )    
-    return render_template('home2.html', title="User signup")
+    return render_template('signup-page.html', title="User Sign Up Page")
 
 @app.route('/welcome')
 def welcome_user():
